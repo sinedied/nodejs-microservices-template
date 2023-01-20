ae6503d00694d7ac6cbcad89c080960fa0304e71
Yohan Lasorsa <noda@free.fr>
2023-01-20 12:25:16 +0000

    Setup CI/CD

--- .azure/build.sh
+++ .azure/build.sh
@@ -2,4 +2,11 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
+# Install dependencies
+npm ci
 
+# Build all Docker images
+npm run docker:build --if-present --workspaces
+
+# Build the website
+npm run build --workspace=website
--- .azure/deploy.sh
+++ .azure/deploy.sh
@@ -1,14 +1,66 @@
 #!/usr/bin/env bash
-set -eu
+set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
-source .settings
 source .prod.env
 cd ..
 
-client_id="$(echo "$AZURE_CREDENTIALS" | jq -r .clientId)"
-client_secret="$(echo "$AZURE_CREDENTIALS" | jq -r .clientSecret)"
-subscription_id="$(echo "$AZURE_CREDENTIALS" | jq -r .subscriptionId)"
-tenant_id="$(echo "$AZURE_CREDENTIALS:" | jq -r .tenantId)"
+# Get current commit SHA
 commit_sha="$(git rev-parse HEAD)"
 
+# Allow silent installation of Azure CLI extensions
+az config set extension.use_dynamic_install=yes_without_prompt
 
+echo "Logging into Docker..."
+echo "$REGISTRY_PASSWORD" | docker login \
+  --username "$REGISTRY_USERNAME" \
+  --password-stdin \
+  "$REGISTRY_NAME.azurecr.io"
+
+echo "Deploying settings-api..."
+docker image tag settings-api "$REGISTRY_NAME.azurecr.io/settings-api:$commit_sha"
+docker image push "$REGISTRY_SERVER/settings-api:$commit_sha"
+
+az containerapp update \
+  --name "${CONTAINER_APP_NAMES[0]}" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --image "$REGISTRY_SERVER/settings-api:$commit_sha" \
+  --set-env-vars \
+    DATABASE_CONNECTION_STRING="$DATABASE_CONNECTION_STRING" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv
+
+echo "Deploying dice-api..."
+docker image tag dice-api "$REGISTRY_NAME.azurecr.io/dice-api:$commit_sha"
+docker image push "$REGISTRY_SERVER/dice-api:$commit_sha"
+
+az containerapp update \
+  --name "${CONTAINER_APP_NAMES[1]}" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --image "$REGISTRY_SERVER/dice-api:$commit_sha" \
+  --set-env-vars \
+    DATABASE_CONNECTION_STRING="$DATABASE_CONNECTION_STRING" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv
+
+echo "Deploying gateway-api..."
+docker image tag gateway-api "$REGISTRY_NAME.azurecr.io/gateway-api:$commit_sha"
+docker image push "$REGISTRY_SERVER/gateway-api:$commit_sha"
+
+az containerapp update \
+  --name "${CONTAINER_APP_NAMES[2]}" \
+  --resource-group "$RESOURCE_GROUP_NAME" \
+  --image "$REGISTRY_SERVER/gateway-api:$commit_sha" \
+  --set-env-vars \
+    SETTINGS_API_URL="https://${CONTAINER_APP_HOSTNAMES[0]}" \
+    DICE_API_URL="https://${CONTAINER_APP_HOSTNAMES[1]}" \
+  --query "properties.configuration.ingress.fqdn" \
+  --output tsv
+
+echo "Deploying website..."
+cd packages/website
+npx swa deploy \
+  --app-name "${STATIC_WEB_APP_NAMES[0]}" \
+  --deployment-token "${STATIC_WEB_APP_DEPLOYMENT_TOKENS[0]}" \
+  --env "production" \
+  --no-use-keychain \
+  --verbose
