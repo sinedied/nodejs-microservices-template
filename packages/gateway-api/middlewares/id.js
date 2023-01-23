d13b0bd0690987fcbfc8b921fc2966d3543be559
Yohan Lasorsa <noda@free.fr>
2023-01-23 19:43:12 +0000

    Update id

--- packages/gateway-api/middlewares/id.js
+++ packages/gateway-api/middlewares/id.js
@@ -1,16 +1,16 @@
+const crypto = require('crypto');
+
 // Middleware to pass on request ID
 function requestId(req, res, next) {
-  const requestId = req.get('x-request-id');
-  if (requestId) {
-    // Set the request ID on the response
-    res.set('x-request-id', requestId);
-    // Create a fetch proxy that sets the request ID on the request
-    req.fetch = (url, options) => {
-      options = options || {};
-      options.headers = options.headers || {};
-      options.headers['x-request-id'] = requestId;
-      return fetch(url, options);
-    }
+  const requestId = req.get('x-request-id') || crypto.randomUUID();
+  // Set the request ID on the response
+  res.set('x-request-id', requestId);
+  // Create a fetch proxy that sets the request ID on the request
+  req.fetch = (url, options) => {
+    options = options || {};
+    options.headers = options.headers || {};
+    options.headers['x-request-id'] = requestId;
+    return fetch(url, options);
   }
   next();
 }
