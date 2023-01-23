84057930ea28b94a0d8944a3ef85476d31227852
Yohan Lasorsa <noda@free.fr>
2023-01-23 20:50:42 +0000

    renema corr id header

--- packages/gateway-api/middlewares/id.js
+++ packages/gateway-api/middlewares/id.js
@@ -2,14 +2,14 @@ const crypto = require('crypto');
 
 // Middleware to pass on request ID
 function requestId(req, res, next) {
-  const requestId = req.get('x-request-id') || crypto.randomUUID();
+  const requestId = req.get('x-correlation-id') || crypto.randomUUID();
   // Set the request ID on the response
-  res.set('x-request-id', requestId);
+  res.set('x-correlation-id', requestId);
   // Create a fetch proxy that sets the request ID on the request
   req.fetch = (url, options) => {
     options = options || {};
     options.headers = options.headers || {};
-    options.headers['x-request-id'] = requestId;
+    options.headers['x-correlation-id'] = requestId;
     return fetch(url, options);
   }
   next();
