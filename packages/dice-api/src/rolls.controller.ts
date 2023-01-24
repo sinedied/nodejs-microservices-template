2fa5c16844d03b5b99184692751052651416b391
Yohan Lasorsa <noda@free.fr>
2023-01-24 11:07:31 +0000

    Fixes

--- packages/dice-api/src/rolls.controller.ts
+++ packages/dice-api/src/rolls.controller.ts
@@ -18,7 +18,7 @@ export class RollsController {
 
   @Post()
   async rollDice(@Body('sides') sides: number) {
-    this.logger.log(`Rolling dice [${sides}]}`);
+    this.logger.log(`Rolling dice [${sides}]`);
     const result = Math.ceil(Math.random() * sides);
     await this.db.addRoll({
       sides: sides,
